dd625020f61ef81646a1ac79ed32f5306a4062fa
Java <Java>
2017-04-10 23:22:20 +0000

    primer commit desde la utn

---
+++ encapsulamiento/src/encapsulamiento/GetsYSets.java
@@ -0,0 +1,13 @@
+package encapsulamiento;
+
+public class GetsYSets {
+
+	
+}
+
+/*
+ * encapsulamiento: Acceder a una variable privada por medio de un metodo publico
+ * public void setSaldo(double saldo){
+ * <contratos          ><firma       >
+ * 
+ */
